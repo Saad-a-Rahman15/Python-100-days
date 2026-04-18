58ca43af0235dd6e4813b2d22a1a5798d2767f13
Saad-A-Rahman15 <114866850+Saad-a-Rahman15@users.noreply.github.com>
2026-04-18 11:58:10 +0100

    wip

--- App_31/main.py
+++ App_31/main.py
@@ -20,7 +20,7 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Minecraft", 40, "italic", "underline"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Minecraft", 40, "italic", "underline"))
 canvas.create_text(400, 263, text="word", font=("Minecraft", 60, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
